3d661e6c1e318fb08a22114de4adc3ad07d2b27c
Hugo Ramon <hramonp-dev@outlook.com>
2024-10-19 23:09:10 -0300

    Refactored the home page by breaking into separate components

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = "Tech People Blog";
 export const SITE_DESCRIPTION = "Articles, news and resources for Tech People";
 
-export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
