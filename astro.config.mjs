8333962a08aa125874244bc829cdce5395e5afb9
Hugo Ramon <hramonp-dev@outlook.com>
2024-10-19 22:26:26 -0300

    Added utils dateFormatter function and reordered the articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon({
-      include: {
-        mdi: ['*'],
-      },
-    }),
-  ],
-})
+  integrations: [tailwind(), icon({})],
+});
